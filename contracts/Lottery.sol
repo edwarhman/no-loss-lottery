8328891a25e016b7ca2ecbf1b9a1e7344fc3ede0
edwarhman <edwarhman@gmail.com>
2022-04-05 12:13:15 -0400

    task-13: change checkUpkeep performUpkeep functions logic

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -13,7 +13,8 @@ contract Lottery is Initializable, AccessControlUpgradeable {
    uint256 public lotteryResult;
    bool public paused;
    Round[] public rounds;
-   mapping(address => mapping(uint256 => uint256)) public participantsFundsByRound;
+   mapping(address => mapping(uint256 => uint256))
+      public participantsFundsByRound;
    uint256 public fee;
    mapping(Asset => address) assetAdress;
 
@@ -47,10 +48,16 @@ contract Lottery is Initializable, AccessControlUpgradeable {
       rounds.push();
       rounds[0].startTime = block.timestamp;
       rounds[0].rewardAsset = Asset.USDC;
+
+      ticketPrice = 10; //10 usd
    }
 
    function participate(uint256 ticketsAmount, Asset payMethod) public payable {
-      Round storage round = rounds[currentRoundId];
+      Round storage round = rounds[
+         currentRoundStatus == RoundStatus.collecting
+            ? currentRoundId
+            : currentRoundId + 1
+      ];
       uint256 allowance = 1 ether; //for test, delete later
 
       require(
@@ -59,10 +66,16 @@ contract Lottery is Initializable, AccessControlUpgradeable {
       );
       uint256 assetToUsd = getPrice(payMethod);
       uint256 totalToPay = assetToUsd * ticketPrice * ticketsAmount;
-      require(totalToPay <= allowance || totalToPay <= msg.value, "Token allowance is too low");
+      require(
+         totalToPay <= allowance || totalToPay <= msg.value,
+         "Token allowance is too low"
+      );
       if (payMethod == Asset.ETH) {
-         if(totalToPay < msg.value) {
-            payable(msg.sender).call{value: msg.value - totalToPay}("");
+         if (totalToPay < msg.value) {
+            (bool success, ) = payable(msg.sender).call{
+               value: msg.value - totalToPay
+            }("");
+            require(success, "refund has failed");
          }
       } else {
          //transferFrom(msg.sender, totalToPay);
@@ -80,52 +93,64 @@ contract Lottery is Initializable, AccessControlUpgradeable {
    }
 
    function withdraw() public {
-      if(hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
-         for(uint i = 0; i < uint(Asset.ETH); i++) {
+      if (hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
+         for (uint256 i = 0; i < uint256(Asset.ETH); i++) {
             //IERC20(AssetAddress[Asset[i]]).transfer(msg.sender, chargesByAsset[Asset[i]]);
          }
       }
 
-      uint[] memory userFunds = new uint[](4);
-      for(uint i = 0; i < rounds.length; i++) {
-         if(participantsFundsByRound[msg.sender][i] > 0) {
-            userFunds[uint(rounds[i].rewardAsset)] += participantsFundsByRound[msg.sender][i];
-            if(msg.sender == rounds[i].winner) {
-               userFunds[uint(rounds[i].rewardAsset)] += rounds[i].reward;
+      uint256[] memory userFunds = new uint256[](4);
+      for (uint256 i = 0; i < rounds.length; i++) {
+         if (participantsFundsByRound[msg.sender][i] > 0) {
+            userFunds[
+               uint256(rounds[i].rewardAsset)
+            ] += participantsFundsByRound[msg.sender][i];
+            if (msg.sender == rounds[i].winner) {
+               userFunds[uint256(rounds[i].rewardAsset)] += rounds[i].reward;
             }
          }
       }
    }
 
-
    function checkUpkeep(bytes calldata checkdata)
-   public
-   view
-   returns (bool, bytes memory)
+      public
+      view
+      returns (bool, bytes memory)
    {
       Round storage current = rounds[currentRoundId];
 
-      if(currentRoundStatus == RoundStatus.collecting) {
-         if(collectTime + current.startTime < block.timestamp) {
-            return (true, abi.encodeWithSignature("investFunds()"));
-         }
-         return (false,"");
-      } else if (currentRoundStatus == RoundStatus.investing) {
-         if(collectTime + investTime + current.startTime < block.timestamp) {
-            return (true, abi.encodeWithSignature("claimLiquidity()"));
-         }
-         return (false,"");
-      } else {
-         if(lotteryResult > 0) {
-            return (true, abi.encodeWithSignature("chooseWinner()"));
-         }
-         return (false,"");
+      if (
+         currentRoundStatus == RoundStatus.collecting &&
+         collectTime + current.startTime < block.timestamp
+      ) {
+         return (true, abi.encodeWithSignature("investFunds()"));
+      } else if (
+         currentRoundStatus == RoundStatus.investing &&
+         collectTime + investTime + current.startTime < block.timestamp
+      ) {
+         return (true, abi.encodeWithSignature("claimLiquidity()"));
+      } else if (lotteryResult > 0) {
+         return (true, abi.encodeWithSignature("chooseWinner()"));
       }
+      return (false, "");
    }
 
    function performUpkeep(bytes calldata performData) external {
-      (bool success, bytes memory result) = address(this).call(performData);
-      require(success == true, "function call has failed");
+      Round storage current = rounds[currentRoundId];
+
+      if (
+         currentRoundStatus == RoundStatus.collecting &&
+         collectTime + current.startTime < block.timestamp
+      ) {
+         investFunds();
+      } else if (
+         currentRoundStatus == RoundStatus.investing &&
+         collectTime + investTime + current.startTime < block.timestamp
+      ) {
+         claimLiquidity();
+      } else if (lotteryResult > 0) {
+         chooseWinner();
+      }
    }
 
    function getCurrentReward() public {}
@@ -134,35 +159,36 @@ contract Lottery is Initializable, AccessControlUpgradeable {
       paused = _paused;
    }
 
-   function swapTokens(Asset from, uint256 amountIn) public returns (uint256) {}
+   function swapTokens(Asset from, uint256 amountIn) public returns (uint256) {
+      return amountIn;
+   }
 
    function investFunds() public {}
 
    function claimLiquidity() internal {
       Round storage current = rounds[currentRoundId];
-      uint total = 1050 * 10**8; // change this for ilendingPool withdraw
-      uint liquidity = total - current.funds;
+      uint256 total = 1050 * 10**8; // change this for ilendingPool withdraw
+      uint256 liquidity = total - current.funds;
 
-      current.reward = liquidity - liquidity * fee / 100;
-   
+      current.reward = liquidity - (liquidity * fee) / 100;
+
       generateLotteryNumber();
    }
 
    function generateLotteryNumber() internal {
-
-      lotteryResult = 1;
+      lotteryResult = 100000000;
    }
 
    function chooseWinner() internal {
       Round storage current = rounds[currentRoundId];
-      current.winner = current.tickets[lotteryResult]; 
+      current.winner = current.tickets[lotteryResult];
    }
 
    function getPrice(Asset asset) internal returns (uint256) {
-      return 100056993;
+      return asset == Asset.ETH ? 27 * 10**13 : 100056993;
    }
 
-   function getTicketOwner(uint ticket) public view returns(address){
+   function getTicketOwner(uint256 ticket) public view returns (address) {
       return rounds[currentRoundId].tickets[ticket];
    }
 }
