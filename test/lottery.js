717ba6dd284f638ef124048728042c1e704a6dbe
edwarhman <edwarhman@gmail.com>
2022-04-05 15:36:55 -0400

    task-16: add participate function test cases

--- test/lottery.js
+++ test/lottery.js
@@ -23,7 +23,6 @@ describe("No Loss Lottery", ()=> {
       it("Should initialize first round correctly", async ()=> {
          let round = await lottery.rounds(0);
 
-         console.log(round);
          expect(round[3])
          .to
          .equal(Asset.USDC);
@@ -31,6 +30,55 @@ describe("No Loss Lottery", ()=> {
    });
 
    describe("Participate function assertions", ()=> {
+
+      it("Should allow to participate in the lottery", async()=> {
+         await lottery.participate(10, Asset.ETH, {value: ethers.utils.parseEther("1")});
+
+         expect(await lottery.getTicketOwner(0))
+         .to
+         .equal(owner.address);
+
+         expect(await lottery.getTicketOwner(9))
+         .to
+         .equal(owner.address);
+
+         expect(await lottery.participantsFundsByRound(owner.address, 0))
+         .to
+         .above(0);
+
+         expect((await lottery.rounds(0))[1])
+         .to
+         .above(0);
+      });
+
+      it("Should allow the user to participate in the next lottery if current is investing funds", async()=> {
+         await lottery.investFunds();
+
+         console.log(await lottery.rounds(0));
+         await lottery.participate(10, Asset.ETH, {value: ethers.utils.parseEther("1")});
+
+         expect(await lottery.getTicketOwner(0))
+         .to
+         .equal(owner.address);
+
+         expect(await lottery.getTicketOwner(9))
+         .to
+         .equal(owner.address);
+
+         console.log(await lottery.rounds(0));
+         console.log(await lottery.participantsFundsByRound(owner.address, 0));
+
+
+         expect(await lottery.participantsFundsByRound(owner.address, 1))
+         .to
+         .above(0);
+
+         expect((await lottery.rounds(1))[1])
+         .to
+         .above(0);
+
+
+      });
 
       it("Should not allow to participate when round status is complete", async()=> {
          await expect(lottery.participate(10, Asset.DAI))
