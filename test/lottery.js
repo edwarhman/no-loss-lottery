65f2b77f13c9e84653f02f63c6bdc60d8ecff3c5
edwarhman <edwarhman@gmail.com>
2022-04-05 22:36:55 -0400

    task-23: add upkeep functions test case

--- test/lottery.js
+++ test/lottery.js
@@ -9,6 +9,11 @@ describe("No Loss Lottery", ()=> {
       USDT: 2,
       ETH: 3,
    };
+   let RoundStatus = {
+      collecting: 0,
+      investing: 1,
+      finished: 2,
+   };
 
    before(async ()=> {
       Lottery = await ethers.getContractFactory("LotteryTest");
@@ -49,6 +54,7 @@ describe("No Loss Lottery", ()=> {
          expect((await lottery.rounds(0))[1])
          .to
          .above(0);
+
       });
 
       it("Should allow the user to participate in the next lottery if current is investing funds", async()=> {
@@ -71,6 +77,62 @@ describe("No Loss Lottery", ()=> {
          .to
          .be
          .revertedWith("Cannot participate, round status is finished");
+      });
+
+      it("Should not allow to participate if there is not sufficient allowance", async ()=> {
+         await expect(lottery.participate(10, Asset.ETH))
+         .to
+         .be
+         .revertedWith("Token allowance is too low");
+      });
+   });
+
+   describe("Upkeep functions assertions", ()=> {
+      let day= 60 * 60 * 24;
+      let collectTime = day * 2;
+      let investTime = day * 5;
+
+      it("Should check round status property and perform functionst property", async()=> {
+         let result;
+
+         await network.provider.send("evm_increaseTime", [collectTime]); 
+         await network.provider.send("evm_mine");
+
+         result = await lottery.checkUpkeep("0x");
+
+         if(result) {
+            await lottery.performUpkeep("0x");
+         }
+
+         expect(await lottery.currentRoundStatus())
+         .to
+         .equal(RoundStatus.investing);
+
+         await network.provider.send("evm_increaseTime", [collectTime + investTime]); 
+         await network.provider.send("evm_mine");
+
+         result = await lottery.checkUpkeep("0x");
+
+         if(result) {
+            await lottery.performUpkeep("0x");
+         }
+
+         expect(await lottery.currentRoundStatus())
+         .to
+         .equal(RoundStatus.finished);
+
+         result = await lottery.checkUpkeep("0x");
+
+         if(result) {
+            await lottery.performUpkeep("0x");
+         }
+
+         expect(await lottery.currentRoundStatus())
+         .to
+         .equal(RoundStatus.collecting);
+
+         
+
       });
    });
    
